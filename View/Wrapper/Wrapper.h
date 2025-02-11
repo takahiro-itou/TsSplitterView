ddb2a9c325c30c8ef8704eddb93b41a6b2dcc6c1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-11 19:35:57 +0900

    コンパイルエラーを修正

--- View/Wrapper/Wrapper.h
+++ View/Wrapper/Wrapper.h
@@ -1,8 +1,20 @@
-﻿// Wrapper.h
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
 
 #pragma once
 
-#include    "Sample/Common/SampleDocument.h"
+#include    "TsSplitter/Common/SampleDocument.h"
 
 using namespace System;
 
@@ -116,7 +128,7 @@ public:
 //
 private:
 
-    typedef     Sample::Common::SampleDocument  WrapTarget;
+    typedef     TsSplitter::Common::SampleDocument  WrapTarget;
 
     WrapTarget  *   m_ptrObj;
 };
